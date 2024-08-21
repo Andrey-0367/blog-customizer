d0c0ec7f9315bb53f68608c950a9b884b728eb00
Andrey-0367 <shestakov0367@yandex.ru>
2024-08-21 18:25:45 +0300

    proverka

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,18 +1,31 @@
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import React from 'react';
 
 /** Функция для обработки открытия/закрытия формы */
-export type OnClick = () => void;
+export type onClick = () => void;
 
-export const ArrowButton = () => {
+type OnClickProps = {
+	onClick?: (state: boolean) => void;
+	isOpen?: boolean;
+};
+
+export const ArrowButton = ({ onClick, isOpen }: OnClickProps) => {
+	const handleClick = () => {
+		onClick?.(!isOpen);
+	};
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
+			className={`${styles.container} ${isOpen ? styles.container_open : ''}`}
+			onClick={(e: React.MouseEvent) => {
+				e.preventDefault();
+				handleClick();
+			}}>
 			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
 		</div>
 	);
